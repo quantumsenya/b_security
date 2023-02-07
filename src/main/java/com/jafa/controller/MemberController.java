1565cc907030312805de45f7c6cb3f1d8634654f
quantumsenya <k_cookie@naver.com>
2023-02-07 10:02:35 +0900

    회원가입서비스

--- src/main/java/com/jafa/controller/MemberController.java
+++ src/main/java/com/jafa/controller/MemberController.java
@@ -1,13 +1,22 @@
 package com.jafa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.jafa.domain.MemberVO;
+import com.jafa.service.MemberService;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
+	@Autowired
+	MemberService memberService;
+	
 	// 로그인 페이지
 	@RequestMapping("/login")
 	public String loginForm() {
@@ -29,8 +38,24 @@ public class MemberController {
 		// 관리자만 허용
 	}
 	
+	// 접근 거부 처리
 	@GetMapping("/accessError")
 	public void accessError() {
-		// 권한없음
+		
+	}
+	
+	// 회원가입 폼
+	@GetMapping("/join")
+	public void joinForm() {
+		
+	}
+	
+	// 회원가입 처리
+	@PostMapping("/join")
+	public String join(MemberVO vo, RedirectAttributes rttr) {
+		memberService.join(vo);
+		rttr.addFlashAttribute("message", "회원가입성공");
+		return "redirect:/";
+		
 	}
 }
